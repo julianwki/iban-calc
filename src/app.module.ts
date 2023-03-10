8a9ef72add8298e5ae4f1e9e2fae35bdf5341e67
Julian Wiersbitzki <jw@agital.online>
2023-03-10 12:14:51 +0100

    remove example app controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { IbanModule } from './iban/iban.module';
 
 @Module({
-  imports: [IbanModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [IbanModule]
 })
 export class AppModule {}
